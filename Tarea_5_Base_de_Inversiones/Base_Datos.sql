de9ec1271687a74f275d37620b49d98a715ef0ac
Esteban Romero Minchaca <423086812@pcpuma.acatlan.unam.mx>
2026-03-22 17:06:30 -0600

    Delete Tarea_5_Base_de_Inversiones/Base_Datos.sql

--- Tarea_5_Base_de_Inversiones/Base_Datos.sql
+++
@@ -1,9 +0,0 @@
--- SQL content for database setup
-CREATE TABLE IF NOT EXISTS Investments (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    amount DECIMAL(10, 2) NOT NULL,
-    date DATE NOT NULL
-);
-
--- Add any additional SQL commands here...
