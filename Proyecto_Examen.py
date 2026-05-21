e5f4ac7963984ba6ccf1ec9b70e95090f8c9dee1
Esteban Romero Minchaca <423086812@pcpuma.acatlan.unam.mx>
2026-05-21 13:47:12 -0600

    EL HILO CONSUMIDOR DE TELEMETRÍA (PROCESAMIENTO I/O BOUND)
    Hilo en segundo plano que extrae datos de la cola y los imprime sin congelar o ralentizar la ventana de la interfaz de usuario.

--- Proyecto_Examen.py
+++ Proyecto_Examen.py
@@ -8,7 +8,19 @@ semaforo_io = threading.Semaphore(1)
 estadisticas_globales = {"renders_totales": 0}
 
 
-
+def consumidor_telemetria():
+    while True:
+        mensaje = cola_telemetria.get()
+        if mensaje == "SALIR":
+            break
+        
+        with semaforo_io:
+            with lock_estadisticas:
+                estadisticas_globales["renders_totales"] += 1
+                total = estadisticas_globales["renders_totales"]
+            print(f"[Log Hilo] Render #{total} -> {mensaje}")
+        
+        cola_telemetria.task_done()
 
 
 
