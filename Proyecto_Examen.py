abc0d4c264e45d3715b3faebfe3d6edbf2e22b2d
Daniel Alberto Luna Escamilla <111752638+danielluna7761@users.noreply.github.com>
2026-05-21 14:21:17 -0600

    aqui esta el nucleo de computo vectorial (matematicas CPU-bound)
    Algoritmo optimizado (vía NumPy) aqui se que calculan las 
    iteraciones del fractal evadiendo los ciclos de Python.

--- Proyecto_Examen.py
+++ Proyecto_Examen.py
@@ -1,6 +1,7 @@
 import threading
 import queue
 import time
+import numpy as np
 
 cola_telemetria = queue.Queue()            
 lock_estadisticas = threading.Lock()       
@@ -33,6 +34,33 @@ def medir_rendimiento(func):
         return resultado, tiempo_total
     return wrapper
 
+
+
+def calcular_bloque_mandelbrot_vectorizado(args):
+    y_start, y_end, ancho, alto, x_min, x_max, y_min, y_max, max_iter = args
+    
+    x_coords = np.linspace(x_min, x_max, ancho)
+    y_coords = np.linspace(y_min + (y_start / alto) * (y_max - y_min), 
+                           y_min + (y_end / alto) * (y_max - y_min), 
+                           y_end - y_start, endpoint=False)
+    
+    c = x_coords + 1j * y_coords[:, np.newaxis]
+    
+    z = np.zeros_like(c, dtype=complex)
+    iteraciones = np.zeros(c.shape, dtype=int)
+    vivos = np.ones(c.shape, dtype=bool)  
+    
+    for i in range(max_iter):
+        if not vivos.any():
+            break
+        z[vivos] = z[vivos] ** 2 + c[vivos]
+        
+        escapados = (z.real * 2 + z.imag * 2 > 4.0) & vivos
+        iteraciones[escapados] = i
+        vivos[escapados] = False
+    
+    bloque_color = (iteraciones * 255 / max_iter).astype(np.uint8)
+    return y_start, y_end, bloque_color
 
 
 
